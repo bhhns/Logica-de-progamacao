9c97930dbc4e7b6ee8f81650bab5cef6a87b8573
bhhns <brunohns13@gmail.com>
2025-08-13 13:11:28 -0300

    começo

---
+++ AULA1/materia1.c
@@ -0,0 +1,11 @@
+/*isso vai servir para poder digitar comentarios a vontade mesmo que quebre linhas*/
+// ou da pra usar isso que funciona
+//usa o #include para importar bibliotecas
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world");
+    return 0;
+}
